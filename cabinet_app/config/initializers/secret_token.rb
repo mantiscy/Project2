620c3b7342a6845e6778d5dfffbd8c0932cd6029
mantiscy <mantiscy@gmail.com>
2014-02-24 13:23:17 +0000

    Empty rails app - initial commit. We should run this in development and put our code in the app, development branch

---
+++ cabinet_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CabinetApp::Application.config.secret_token = '0e74bbebf449b51e31333dd9a57d8a35c6d10b20d1a6f80865408447830e5ca4a3222c59a91d7043e670dfeea9aa71d88ebfb88a5f5ae1304a2a5888e610855e'
