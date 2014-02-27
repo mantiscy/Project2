7c9cac839a80e8f021e7963b032467b682d04993
Clemens Kazda <clemkaz@Clemenss-MacBook-Pro.local>
2014-02-27 18:41:02 +0000

    Email Notification after sharing network working

--- cabinet_app/app/controllers/locations_controller.rb
+++ cabinet_app/app/controllers/locations_controller.rb
@@ -92,6 +92,7 @@ class LocationsController < ApplicationController
       loc.users << user
       #@location.save
       @locations = current_user.locations
+      puts "<<<<<<<<<<<<<<<< #{user.first.email}"
       UserMailer.share_location(user.first, loc).deliver
       respond_to do |format|
         if request.xhr?
