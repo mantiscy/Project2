d13db0af5cefa6479b48e73e98476b4df9acc0dd
Clemens Kazda <clemkaz@Clemenss-MacBook-Pro.local>
2014-02-27 17:11:32 +0000

    Changes to Footer. HTML and In Line Attachments in Email

--- cabinet_app/app/mailers/user_mailer.rb
+++ cabinet_app/app/mailers/user_mailer.rb
@@ -4,6 +4,8 @@ class UserMailer < ActionMailer::Base
   def registration_confirmation(user)
     @user = user
     attachments['railslogo'] = File.read("#{Rails.root}/app/assets/images/rails.png")
+    attachments['pin'] = File.read("#{Rails.root}/public/ex_pin.png")
+    attachments.inline['logo2'] = File.read("#{Rails.root}/public/logo2.png")
     mail(to: user.email, subject: 'Hello new user!')
   end
 
@@ -11,5 +13,10 @@ class UserMailer < ActionMailer::Base
     #@user = user
     #attachments['railslogo'] = File.read("#{Rails.root}/app/assets/images/rails.png")
     mail(to: user.email, subject: 'Shared location')
+  end
+
+  def welcome
+    attachments.inline['ex_pin.png'] = File.read('/public/ex_pin.png')
+    attachments.inline['logo2.png'] = File.read('/public/logo2.png')
   end
 end
--- cabinet_app/config/initializers/devise.rb
+++ cabinet_app/config/initializers/devise.rb
@@ -228,7 +228,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
-  config.omniauth :facebook, '246222115557748', 'be917219c70c030587d61e329870e5ff'
+  config.omniauth :facebook, '1471196156434107', '4b1e4c73d62323b16343b0c2246640bf'
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
