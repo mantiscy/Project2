266ec13d9e668779394d5387b67c8276c9b196fa
mantiscy <mantiscy@gmail.com>
2014-03-01 11:04:50 +0000

    heroku stuff

--- cabinet_app/config/environments/production.rb
+++ cabinet_app/config/environments/production.rb
@@ -45,6 +45,24 @@ Contacts::Application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
 
+  # Don't care if the mailer can't send
+config.action_mailer.raise_delivery_errors = true
+
+# Change mail delvery to either :smtp, :sendmail, :file, :test
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "gmail.com",
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: ENV["GMAIL_USERNAME"],
+  password: ENV["GMAIL_PASSWORD"]
+}
+
+# Specify what domain to use for mailer URLs
+config.action_mailer.default_url_options = {host: "wificabinet.herokuapp.com"}
+
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
 
